6a084da59ddd1229a62b08b62667d1860218ae32
Ashley <59657132+sillyash@users.noreply.github.com>
2025-02-13 17:26:19 +0000

    Refactor layout structure by adding a main wrapper in App.vue and simplifying Portail.vue

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,27 @@ import AppFooter from '@/components/AppFooter.vue'
 
 <template>
   <AppHeader />
-  <RouterView />
+
+  <main id="mainApp">
+    <RouterView />
+  </main>
+
   <AppFooter />
 </template>
 
 <style>
 @import '@/styles/main.css';
+
+#mainApp {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(
+    100vh
+    - var(--header-height) - var(--footer-height)
+    - 2* var(--header-padding) - 2 * var(--footer-padding)
+  );
+}
+
 </style>
--- src/views/Portail.vue
+++ src/views/Portail.vue
@@ -1,22 +1,9 @@
 <template>
-    <main id="mainPortail">
-        <h1>Portail - IUT Orsay</h1>
-    </main>
+    <h1>Portail - IUT Orsay</h1>
 </template>
 
 <style lang="css" scoped>
 @import '@/styles/main.css';
 
-#mainPortail {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
 
-    padding-left: var(--main-padding);
-    padding-right: var(--main-padding);
-    padding-top: calc(var(--header-height) + 2 * var(--padding) + 15px);
-    padding-bottom: calc(var(--header-height) + 2 * var(--padding) + 15px);
-}
 </style>
