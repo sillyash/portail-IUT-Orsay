22b9bc6a40ce2fa32de40d8118eb172b21658441
Ashley <merienne.ashley@gmail.com>
2025-04-22 23:07:19 +0200

    Set base path for Vite configuration to support deployment structure

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/portail-IUT-Orsay/',
   plugins: [
     vue(),
     vueDevTools(),
